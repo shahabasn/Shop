5cd6c6f38363ad2709e791ebd1b232664052110c
shahabasn <shahabasneyyan@gmail.com>
2026-06-14 22:38:06 +0530

    Switch to Django StaticFilesStorage to bypass WhiteNoise post-processing/compression errors during deploy

--- qr_menu/settings.py
+++ qr_menu/settings.py
@@ -127,13 +127,13 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
 # Compatibility settings for older packages (like django-cloudinary-storage) under Django 6.x
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
